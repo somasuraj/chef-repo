44761a76f41ff352b987ecf81cc069b4354cf27e
Suraj Soma <ssoma@videologygroup.com>
2016-03-09 19:36:55 +0530

    Initial Commit

---
+++ cookbooks/apache2/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: apache2
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "apache2" do 
+	action :install
+end
